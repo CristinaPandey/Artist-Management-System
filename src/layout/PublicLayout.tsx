a8239a37b39a50cace129caa09d4726d22c21c93
Cristina Pandey <104251773+CristinaPandey@users.noreply.github.com>
2025-04-17 22:39:49 +0545

    signup , dashboard route setup

--- src/layout/PublicLayout.tsx
+++ src/layout/PublicLayout.tsx
@@ -4,26 +4,26 @@ import { Outlet, useNavigate, Navigate } from "react-router-dom";
 
 export default function PublicLayout() {
   const navigate = useNavigate();
-  const accessToken = localStorage.getItem("access_token");
+  // const accessToken = localStorage.getItem("access_token");
 
-  useEffect(() => {
-    if (!accessToken) {
-      navigate("/", { replace: true });
-      return;
-    }
-  }, [accessToken, navigate]);
+  // useEffect(() => {
+  //   if (!accessToken) {
+  //     navigate("/", { replace: true });
+  //     return;
+  //   }
+  // }, [accessToken, navigate]);
 
   return (
     <>
-      {!accessToken ? (
-        <Box sx={{ display: "flex" }}>
-          <Box sx={{ width: { xs: "100%", lg: "100%" } }}>
-            <Outlet />
-          </Box>
+      {/* {!accessToken ? ( */}
+      <Box sx={{ display: "flex" }}>
+        <Box sx={{ width: { xs: "100%", lg: "100%" } }}>
+          <Outlet />
         </Box>
-      ) : (
+      </Box>
+      {/* ) : (
         <Navigate to="/dashboard" replace={true} />
-      )}
+      )} */}
     </>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -18,6 +18,7 @@ import { Visibility, VisibilityOff } from "@mui/icons-material";
 import RoundedButton from "../components/Button/Button";
 import SuccessBar from "../components/Snackbar/SuccessBar";
 import ErrorBar from "../components/Snackbar/ErrorBar";
+import { Link, useNavigate } from "react-router-dom";
 // import { useLoginMutation } from "services/Auth/AuthServices";
 
 export interface LoginData {
@@ -30,7 +31,8 @@ const schema = yup.object({
   password: yup.string().required("Password is required"),
 });
 
-const Login = () => {
+const Login: React.FC = () => {
+  const navigate = useNavigate();
   const theme = useTheme();
   const {
     control,
@@ -48,30 +50,34 @@ const Login = () => {
 
   //   const { mutate: AuthMutation, isPending } = useLoginMutation();
 
-  //   const handleLogin = async (data: LoginData) => {
-  //     const payload = {
-  //       username: data?.username,
-  //       password: btoa(data?.password),
-  //     };
+  const handleLogin = async (data: LoginData) => {
+    //     const payload = {
+    //       username: data?.username,
+    //       password: btoa(data?.password),
+    //     };
+    //     AuthMutation(payload, {
+    //       onSuccess: () => {
+    //         setSuccessMessage("Login Successful!");
+    //         setOpenError(false);
+    //         setOpenSuccess(true);
+    //       },
+    //       onError: (error) => {
+    //         if (isAxiosError(error) && error.response) {
+    //           setErrorMessage(
+    //             error.response.data.responseData
+    //               ? error.response.data.responseData.detail
+    //               : `Error Occured while logging in.`
+    //           );
+    //           setOpenError(true);
+    //         }
+    //       },
+    //     });
+  };
 
-  //     AuthMutation(payload, {
-  //       onSuccess: () => {
-  //         setSuccessMessage("Login Successful!");
-  //         setOpenError(false);
-  //         setOpenSuccess(true);
-  //       },
-  //       onError: (error) => {
-  //         if (isAxiosError(error) && error.response) {
-  //           setErrorMessage(
-  //             error.response.data.responseData
-  //               ? error.response.data.responseData.detail
-  //               : `Error Occured while logging in.`
-  //           );
-  //           setOpenError(true);
-  //         }
-  //       },
-  //     });
-  //   };
+  const handleOnClick = (e: React.MouseEvent) => {
+    e.preventDefault();
+    navigate("/register", { replace: true });
+  };
 
   return (
     <React.Fragment>
@@ -85,103 +91,119 @@ const Login = () => {
         setSnackbarOpen={setOpenError}
         message={errorMessage}
       />
-      {/* <form onSubmit={handleSubmit(handleLogin)}> */}
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          height: "100vh",
-          //   backgroundImage: `url(${Background})`,
-          backgroundPosition: "center",
-          backgroundRepeat: "no-repeat",
-          backgroundSize: "cover",
-          backgroundColor: theme.palette.primary.light,
-        }}
-      >
+      <form onSubmit={handleSubmit(handleLogin)}>
         <Box
           sx={{
-            py: 4,
-            px: 8,
             display: "flex",
-            flexDirection: "column",
-            gap: 2,
-            width: "400px",
-            backgroundColor: "#fff",
-            borderRadius: "10px",
+            alignItems: "center",
+            justifyContent: "center",
+            height: "100vh",
+            //   backgroundImage: `url(${Background})`,
+            backgroundPosition: "center",
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundColor: theme.palette.primary.light,
           }}
         >
-          <Box sx={{ alignSelf: "center" }}>{/* <img src={Logo} /> */}</Box>
-          <Box>
+          <Box
+            sx={{
+              py: 4,
+              px: 8,
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              width: "400px",
+              backgroundColor: "#fff",
+              borderRadius: "10px",
+            }}
+          >
+            <Box sx={{ alignSelf: "center" }}>{/* <img src={Logo} /> */}</Box>
+            <Box>
+              <Typography
+                sx={{ color: theme.palette.secondary.main, fontWeight: 600 }}
+              >
+                Login to your account
+              </Typography>
+            </Box>
+
+            <Controller
+              name="username"
+              control={control}
+              defaultValue=""
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  size="small"
+                  fullWidth
+                  placeholder="Username"
+                  error={!!errors.username}
+                  helperText={errors.username?.message}
+                />
+              )}
+            />
+
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              rules={{ required: true }}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  type={showPassword ? "text" : "password"}
+                  size="small"
+                  fullWidth
+                  placeholder="Password"
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <IconButton
+                          onClick={() => setShowPassword((show) => !show)}
+                        >
+                          {showPassword ? (
+                            <VisibilityOff fontSize="small" />
+                          ) : (
+                            <Visibility fontSize="small" />
+                          )}
+                        </IconButton>
+                      </InputAdornment>
+                    ),
+                  }}
+                  error={!!errors.password}
+                  helperText={errors.password?.message}
+                />
+              )}
+            />
+
+            <Box sx={{ alignSelf: "center" }}>
+              <Box sx={{ display: "flex", justifyContent: "center" }}>
+                <RoundedButton
+                  title1="Login"
+                  //   loading={isPending}
+                  //   disable1={isPending}
+                />
+              </Box>
+            </Box>
             <Typography
-              sx={{ color: theme.palette.secondary.main, fontWeight: 600 }}
+              onClick={handleOnClick}
+              variant="body2"
+              sx={{
+                textAlign: "center",
+                mt: 2,
+                cursor: "pointer",
+                fontWeight: 550,
+                color: theme.palette.secondary.main,
+                "&:hover": {
+                  color: theme.palette.secondary.dark,
+                },
+              }}
             >
-              Login to your account
+              Don't have an account?
             </Typography>
           </Box>
-
-          <Controller
-            name="username"
-            control={control}
-            defaultValue=""
-            rules={{ required: true }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                size="small"
-                fullWidth
-                placeholder="Username"
-                error={!!errors.username}
-                helperText={errors.username?.message}
-              />
-            )}
-          />
-
-          <Controller
-            name="password"
-            control={control}
-            defaultValue=""
-            rules={{ required: true }}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                type={showPassword ? "text" : "password"}
-                size="small"
-                fullWidth
-                placeholder="Password"
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        onClick={() => setShowPassword((show) => !show)}
-                      >
-                        {showPassword ? (
-                          <VisibilityOff fontSize="small" />
-                        ) : (
-                          <Visibility fontSize="small" />
-                        )}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-                error={!!errors.password}
-                helperText={errors.password?.message}
-              />
-            )}
-          />
-
-          <Box sx={{ alignSelf: "center" }}>
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              {/* <RoundedButton
-                  title1="Login"
-                  loading={isPending}
-                  disable1={isPending}
-                /> */}
-            </Box>
-          </Box>
         </Box>
-      </Box>
-      {/* </form> */}
+      </form>
     </React.Fragment>
   );
 };
