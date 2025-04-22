691ac8a5391921227a44184187741160bbc1a367
Cristina Pandey <104251773+CristinaPandey@users.noreply.github.com>
2025-04-22 16:46:15 +0545

    minoe changes

---
+++ src/services/DashboardServices.tsx
@@ -0,0 +1,14 @@
+// import { useQuery } from "@tanstack/react-query";
+// import { axiosInstance } from "../config/axiosInstance";
+
+// const getDashboardInfo = async () => {
+//   const response = await axiosInstance.get(`/api/dashboard`);
+//   return response.data;
+// };
+// export const useGetDashboardInfo = () => {
+//   return useQuery({
+//     queryKey: ["dashboard"],
+//     queryFn: () => getDashboardInfo(),
+//     // retry: false,
+//   });
+// };
